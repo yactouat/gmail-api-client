52ec05a3972f049a249eba6a1c1bd24bd7e2b877
yactouat <yactouat@yactouat.com>
2024-08-17 17:41:23 +0200

    Worksplace GMail API client ready

--- README.md
+++ README.md
@@ -2,24 +2,63 @@
 
 A generic emailing system that use GMail Workspace API to send emails.
 
+It listens to a queue of emails to send and sends them using the Gmail API. It also takes into account whatever Workspace plan rate limits for sending emails are in place.
+
 ## pre requisites
 
 We'll assume you run Ubuntu 24 and Python 3.12.
 
 - have Google Workspace enabled
-- have a GCP project with the Gmail API enabled
+- have a GCP project with the Gmail API, the Google Cloud Logging, and the Error Reporting APIs enabled
 - be the administrator of both the Google Workspace and the GCP project
-- Node.js and NPM installed
-- PostgreSQL installed and running with a user and a database created
+- Redis installed
 
 ## setup
 
 - create a service account following [these instructions](https://developers.google.com/workspace/guides/create-credentials?authuser=1#service-account); however do not assign it any role yet
 - add yourself the role of Organization Policy Administrator in the GCP (in the IAM, at organization level)
-- allow for the creation of service accounts JSON keys within GCP Console IAM -> Organization policies -> search for `iam.disableServiceAccountKeyCreation` and disable it
-- create the [JSON key for the service account](https://developers.google.com/workspace/guides/create-credentials?authuser=1#create_credentials_for_a_service_account) and download the JSON key and save it in the root of this project with the name `gmail-api-client-creds.json` (gitignored)
+- allow for the creation of service accounts JSON keys within project GCP Console IAM -> Organization policies -> search for `iam.disableServiceAccountKeyCreation` and disable it
+- create the [JSON key for the service account](https://developers.google.com/workspace/guides/create-credentials?authuser=1#create_credentials_for_a_service_account) and download the JSON key and save it in the root of this project with the name `gcp-creds.json` (gitignored)
 - add the service account [domain wide delegation](https://developers.google.com/workspace/guides/create-credentials?authuser=1#optional_set_up_domain-wide_delegation_for_a_service_account) with the scope `https://mail.google.com/`
+- the service account also needs to have the following permissions for error reporting and logging:
+
+  - `errorreporting.errorEvents.create`
+  - `logging.logEntries.create`
+
 - create a virtual environment with `python3 -m venv venv` and activate it with `source venv/bin/activate`
 - install the dependencies with `pip install -r requirements.txt`
-- `cp .env.example .env` and fill in the environment variables
-- migrate the DB with `python3 migrate.py`
+- `cp .env.example .env` and fill in the environment variables with the relevant values
+- run the app with `python3 app.py`
+
+Now that everything is working, you'll want to create a service for the app, so that it can run in the background and restart automatically in case of a crash:
+
+- `sudo nano /etc/systemd/system/gmail_api_client.service` and add the following content:
+
+```bash
+[Unit]
+Description=GMail API Client
+After=network.target
+
+[Service]
+User=$USER
+WorkingDirectory=/gmail_api_client/path
+ExecStart=/gmail_api_client/path/venv/bin/python3 /gmail_api_client/path/app.py
+Restart=always
+RestartSec=5
+Environment="PATH=/gmail_api_client/path/venv/bin"
+
+# system fully booted and network is up
+[Install]
+WantedBy=multi-user.target
+```
+
+(replace user and paths with your own)
+
+After that, let's start the service and check that everything works as expected:
+
+```bash
+sudo systemctl daemon-reload
+sudo systemctl start gmail_api_client
+sudo systemctl enable gmail_api_client
+sudo systemctl status gmail_api_client
+```
--- app.py
+++ app.py
@@ -1,51 +1,131 @@
+#!/usr/bin/env python3
+
 import base64
 from dotenv import load_dotenv
 from email.message import EmailMessage
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+import json
 import os.path
+import redis
+import time
+from ylogging.lib import get_gcp_error_reporting_logging_clients, get_structured_log_msg
+
+abs_path = os.path.dirname(os.path.abspath(__file__))
 
 
-def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
+def send_email(to: str, subject: str, message: str):
     creds = None
-    path_to_sa_key = "gmail-api-client-creds.json"
+    path_to_sa_key = "gcp-creds.json"
     if os.path.exists(path_to_sa_key):
         creds = service_account.Credentials.from_service_account_file(
             path_to_sa_key, scopes=["https://mail.google.com/"]
-        ).with_subject(os.environ.get("EMAIL_SUBJECT"))
+        ).with_subject(os.environ.get("EMAIL_FROM_ADDRESS"))
+    else:
+        raise Exception("no Google credentials found")
 
-    try:
-        service = build("gmail", "v1", credentials=creds)
-        message = EmailMessage()
+    service = build("gmail", "v1", credentials=creds)
+    message_obj = EmailMessage()
 
-        message.set_content("test message body")
+    message_obj.set_content(message)
 
-        message["To"] = os.environ.get("EMAIL_SUBJECT")
-        message["From"] = os.environ.get("EMAIL_SUBJECT")
-        message["Subject"] = "test message subject"
+    message_obj["To"] = to
+    message_obj["From"] = os.environ.get("EMAIL_FROM_ADDRESS")
+    message_obj["Subject"] = subject
 
-        # encoded message
-        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+    # encoded message
+    encoded_message = base64.urlsafe_b64encode(message_obj.as_bytes()).decode()
 
-        create_message = {"raw": encoded_message}
-        send_message = (
-            service.users()
-            .messages()
-            .send(userId="me", body=create_message)
-            .execute()
-        )
-        print(f'Message Id: {send_message["id"]}')
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        send_message = None
-
+    create_message = {"raw": encoded_message}
+    send_message = (
+        service.users().messages().send(userId="me", body=create_message).execute()
+    )
     return send_message
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    main()
+    load_dotenv(f"{abs_path}/.env")
+
+    email_queue_name = "email_queue"
+    service_name = "gmail-api-client"
+
+    error_reporting_client, logger = get_gcp_error_reporting_logging_clients(
+        service_name
+    )
+
+    logger.log_struct(
+        get_structured_log_msg(service_name, "starting emailing service", None),
+        severity="INFO",
+    )
+
+    email_queue_day_counter_key = "email_queue_day_counter"
+
+    while True:
+        try:
+            # connect to redis
+            redis_client = redis.Redis(
+                host=os.environ.get("REDIS_HOST"),
+                port=int(os.environ.get("REDIS_PORT")),
+                db=int(os.environ.get("REDIS_DATABASE")),
+            )
+
+            emails = redis_client.lrange(email_queue_name, 0, -1)
+
+            for email_data in emails:
+                email = json.loads(email_data)
+                sent = send_email(email["to"], email["subject"], email["message"])
+                logger.log_struct(
+                    get_structured_log_msg(
+                        service_name, "email sent with id", sent["id"]
+                    ),
+                    severity="INFO",
+                )
+                # removing the email from the queue
+                redis_client.lpop(email_queue_name)
+                # increment the day counter
+                num_emails_sent = redis_client.incr(email_queue_day_counter_key)
+                # get the number of seconds before the end of the day
+                SECONDS_IN_A_DAY = 86400
+                seconds_until_end_of_day = (
+                    SECONDS_IN_A_DAY - time.time() % SECONDS_IN_A_DAY
+                )
+                # handle the midnight edge case + reset the counter
+                if seconds_until_end_of_day == SECONDS_IN_A_DAY:
+                    seconds_until_end_of_day = 0
+                    redis_client.set(email_queue_day_counter_key, 0)
+                wait_time = round(
+                    seconds_until_end_of_day
+                    / (
+                        int(os.environ.get("WORKSPACE_PLAN_GMAIL_DAILY_LIMIT"))
+                        - num_emails_sent
+                    )
+                )
+                logger.log_struct(
+                    get_structured_log_msg(
+                        service_name,
+                        "waiting for next email",
+                        json.dumps(
+                            {
+                                "seconds_until_end_of_day": seconds_until_end_of_day,
+                                "num_emails_sent": num_emails_sent,
+                                "wait_time": wait_time,
+                                "workspace_plan_gmail_daily_limit": int(
+                                    os.environ.get("WORKSPACE_PLAN_GMAIL_DAILY_LIMIT")
+                                ),
+                            }
+                        ),
+                    ),
+                    severity="INFO",
+                )
+                if wait_time == 0:
+                    # minimum wait time is 0.1 seconds (to not overload Redis)
+                    wait_time = 0.1
+                # wait x seconds before sending the next email
+                time.sleep(wait_time)
+
+        except Exception as e:
+            logger.log_struct(
+                get_structured_log_msg(service_name, "error sending email", str(e)),
+                severity="ERROR",
+            )
+            error_reporting_client.report_exception()
